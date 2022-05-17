adf16f7e16d5a7e8e51974e2dc9354b1e9289313
MarijaJankovicc <jankovic.ra138.2018@uns.ac.rs>
2022-05-17 23:37:46 +0200

    feat: Field validation

--- dislinkt-front-end/src/app/components/registration/registration.component.ts
+++ dislinkt-front-end/src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -10,27 +10,34 @@ import {FormControl, Validators} from '@angular/forms';
 
 export class RegistrationComponent implements OnInit {
 
-  hide1 = true;
-  hide2 = true;
-  email = new FormControl('', [Validators.required, Validators.email]);
-  
+  //hide1 = true;
+  //hide2 = true;
+
+  registerForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$')]),
+    confirmPassword: new FormControl('', Validators.required)
+  }, { validators: confirmPasswordValidator});
+
   constructor() { }
 
   ngOnInit(): void {
-    
+   
   }
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
+  onSubmit(): void {
+    if (this.registerForm.invalid) {
+      return;
     }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  signUp(): void {
-    console.log("Test Sign Up")
+    // continue work here
   }
   
-
 }
+
+export const confirmPasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  return password && confirmPassword && password.value === confirmPassword.value ? { confirmPassword: true } : { confirmPassword: false };
+};
