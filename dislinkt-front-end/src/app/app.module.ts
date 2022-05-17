b8123e78ae295202e667962377248437a432eb3a
Dejan Bjelic <dejan.bjelic@uns.ac.rs>
2022-05-17 20:10:43 +0200

    feat: Registration form (WIP)

--- dislinkt-front-end/src/app/app.module.ts
+++ dislinkt-front-end/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { MatIconModule} from '@angular/material/icon';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { HomeComponent } from './components/home/home.component'
+import { MatFormFieldModule, MatFormFieldAppearance} from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -30,7 +34,11 @@ import { HomeComponent } from './components/home/home.component'
     MatListModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- dislinkt-front-end/src/app/components/registration/registration.component.ts
+++ dislinkt-front-end/src/app/components/registration/registration.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
+
+
 export class RegistrationComponent implements OnInit {
 
+  hide1 = true;
+  hide2 = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  signUp(): void {
+    console.log("Test Sign Up")
+  }
+  
 
 }
