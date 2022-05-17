f4e9d50e1edc4b65956ef7b12a501d32131d6b06
Dejan Bjelic <dejan.bjelic@uns.ac.rs>
2022-05-17 19:16:46 +0200

    feat: Added homepage, login and registartion

--- dislinkt-front-end/src/app/app.module.ts
+++ dislinkt-front-end/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MatButtonModule } from '@angular/material/button'
+import { MatMenuModule} from '@angular/material/menu'
+import { MatListModule } from '@angular/material/list'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule} from '@angular/material/toolbar'
+import { MatIconModule} from '@angular/material/icon';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { HomeComponent } from './components/home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ dislinkt-front-end/src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("eeeh")
+  }
+
+  login(): void {
+    console.log("Ok button works")
+  }
+
+}
