421584e4c55a107cd5ab7bef05f9838545a1c787
Dejan Bjelic <dejan.bjelic@uns.ac.rs>
2022-05-21 05:16:33 +0200

    feat: Log out

--- dislinkt-front-end/src/app/service/authentication.service.ts
+++ dislinkt-front-end/src/app/service/authentication.service.ts
@@ -2,33 +2,53 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable, tap } from "rxjs";
 import { environment } from 'src/environments/environment';
+import {Router, ActivatedRoute } from "@angular/router";
+
 
 @Injectable({
     providedIn: 'root'
   })
 
+  
+
 export class AuthenticationService {
 
-    constructor(private http: HttpClient) { 
+    
+    constructor(private http: HttpClient, private router: Router,  private activeRoute: ActivatedRoute) { 
       const token = localStorage.getItem("regUserToken")
       this._isLoggedIn$.next(!!token)
     }
 
     private _isLoggedIn$ = new BehaviorSubject<boolean>(false)
     isLoggedIn$ = this._isLoggedIn$.asObservable()
-
+   
    public login(obj: any): Observable<any>{
+        this.router.navigate(['/'])
         return this.http.post(environment.baseUrlAuthService + "/login", obj).pipe(
           tap((response: any) => {
             
             this.storeToken(response.accessToken)
-            console.log(response.accessToken)})
+            console.log(response.accessToken)})                             
         )
+        
     }
 
     public storeToken(accessToken: any) {
       this._isLoggedIn$.next(true)
       localStorage.setItem("regUserToken", accessToken)
+    }
+
+    public logout() {
+      //this.router.navigate(['/login'])
+      window.location.href="http://localhost:4200/login"
+      localStorage.removeItem("regUserToken")
+    }
+
+    reloadCurrentPage(){
+      let currentUrl = this.router.url;
+      this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+      this.router.navigate([currentUrl]);
+      });
     }
 
 }
