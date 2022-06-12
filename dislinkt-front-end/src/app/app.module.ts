cd4da573464a22571319e51c5caad54ca62d2779
Dejan Bjelic <dejan.bjelic@uns.ac.rs>
2022-06-12 13:45:04 +0200

    refactor: Deleted unused import

--- dislinkt-front-end/src/app/app.module.ts
+++ dislinkt-front-end/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { MatIconModule} from '@angular/material/icon';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { HomeComponent } from './components/home/home.component'
-import { MatFormFieldModule, MatFormFieldAppearance} from '@angular/material/form-field'
+import { MatFormFieldModule} from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
